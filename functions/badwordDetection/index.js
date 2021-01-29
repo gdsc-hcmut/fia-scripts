49961e3bcfc55d640cd8f5146f3ca68746d04cb6
TranHoangViet <viet14042000@gmail.com>
2021-01-29 09:31:10 +0700

    end badword statistics

--- functions/badwordDetection/index.js
+++ functions/badwordDetection/index.js
@@ -8,26 +8,35 @@ exports.badwordDetection = functions.database.ref("/messages/{pushId}")
             return null;
         }
         // Grab the current value of what was written to the Realtime Database.
-        const prevMess = change.before.val();
+        // const prevMess = change.before.val();
         let currMess = change.after.val();
 
         // Only check if currData is different from prevMess or prevMess is null
-        if (prevMess && prevMess.message === currMess.message) {
-            return null;
-        }
+        // if (prevMess && prevMess.message === currMess.message) {
+        //     return null;
+        // }
 
         // Get list of bad words from firestore
         const badwordListRef = admin.firestore()
             .collection("badwordCollection").doc("badwordDoc");
         const data = await badwordListRef.get();
+        const badwordLst = data.data().lstBadword
+            .sort((ele1, ele2) => ele2.length - ele1.length);
 
+        let numOfBadword = 0;
         // Replace each badword with ****
-        for (const badword of data.data().lstBadword) {
+        for (const badword of badwordLst) {
             const badwordregex = new RegExp(badword, "g");
             const replaceWord = "*".repeat(badword.length);
+            numOfBadword += (currMess.match(badwordregex) || []).length;
             currMess = currMess.replace(badwordregex, replaceWord);
         }
 
-        // Save updated message
-        return change.after.ref.set({ ...currMess, message: currMess });
+        // Save statistic
+        // return change.after.ref.set({ ...currMess, message: currMess });
+        return admin.firestore().collection("statictisCollection")
+            .doc("statictisDoc").set({
+                [context.auth.uid]:
+                    admin.firestore.FieldValue.increment(numOfBadword),
+            }, { merge: true });
     });
